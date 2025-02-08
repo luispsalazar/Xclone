1b82087c386145664de8313c81c94cea89227c9c
Luis Escobar <luispsalazar@yahoo.com>
2025-02-07 19:56:57 -0500

    First updates in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className='text-5xl text-red-500 bg-blue-300'>Hello from App.jsx</p>
-    </>
-  )
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
+  );
 }
 
 export default App;
